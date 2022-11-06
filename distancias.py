95da46536f28da46635b47167b0e449d7f758f2a
CheesyNacho10 <67198608+CheesyNacho10@users.noreply.github.com>
2022-11-06 20:23:10 +0100

    damerau_matriz y damerau_edicion hecho y tested

--- distancias.py
+++ distancias.py
@@ -135,13 +135,103 @@ def levenshtein_cota_optimista(x, y, threshold):
 def damerau_restricted_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein restringida con matriz
     lenX, lenY = len(x), len(y)
-    # COMPLETAR
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1): # Rellena la primera fila
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1): # Para cada columna
+        D[0][j] = D[0][j - 1] + 1 # Rellenas su vertical
+        for i in range(1, lenX + 1): # Para cada fila
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+            if (i > 1 and j > 1)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 2] + 1
+                )
+                
     return D[lenX, lenY]
 
 def damerau_restricted_edicion(x, y, threshold=None):
     # partiendo de damerau_restricted_matriz añadir recuperar
     # secuencia de operaciones de edición
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1): # Rellena la primera fila
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1): # Para cada columna
+        D[0][j] = D[0][j - 1] + 1 # Rellenas su vertical
+        for i in range(1, lenX + 1): # Para cada fila
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+            if (i > 1 and j > 1)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 2] + 1
+                )
+    indX, indY = D.shape[0] - 1, D.shape[1] - 1
+    op = []
+    while indX > 0 and indY > 0:
+        ins = D[indX, indY - 1]
+        bor = D[indX - 1, indY]
+        sus = D[indX - 1, indY - 1]
+        if (indX > 1 and indY > 1)\
+                and (x[indX - 2] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
+            int = D[indX - 2, indY - 2]
+        else:
+            int = float('inf')
+        opMin = min(ins, bor, sus, int)
+        if (int == opMin):
+            op.append('int')
+            indX -= 2
+            indY -= 2
+        elif (sus == opMin):
+            op.append('sus')
+            indX -= 1
+            indY -= 1
+        elif (ins == opMin):
+            op.append('ins')
+            indY -= 1
+        elif (bor == opMin):
+            op.append('bor')
+            indX -= 1
+    # Caso llego a una pared de la matriz
+    while indY > 0:
+        op.append('ins')
+        indY -= 1
+    while indX > 0:
+        op.append('bor')
+        indX -= 1
+
+    # Recuperación del camino
+    op = op[::-1]
+    indX, indY = 0, 0
+    #print(op)
+    secOp = []
+    for o in op:
+        if (o == 'ins'):
+            secOp.append(('', y[indY]))
+            indY += 1
+        elif (o == 'bor'):
+            secOp.append((x[indX], ''))
+            indX += 1
+        elif (o == 'sus'):
+            secOp.append((x[indX], y[indY]))
+            indX += 1
+            indY += 1
+        elif (o == 'int'):
+            secOp.append((str(x[indX]) + str(x[indX + 1]), str(y[indY]) + str(y[indY + 1])))
+            indX += 2
+            indY += 2
+        #print(secOp)
+    return D[lenX, lenY], secOp
 
 def damerau_restricted(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
@@ -166,7 +256,7 @@ opcionesSpell = {
     'levenshtein_r': levenshtein_reduccion,
     'levenshtein':   levenshtein,
     'levenshtein_o': levenshtein_cota_optimista,
-    #'damerau_rm':    damerau_restricted_matriz,
+    'damerau_rm':    damerau_restricted_matriz,
     #'damerau_r':     damerau_restricted,
     #'damerau_im':    damerau_intermediate_matriz,
     #'damerau_i':     damerau_intermediate
@@ -175,6 +265,6 @@ opcionesSpell = {
 opcionesEdicion = {
     'levenshtein': levenshtein_edicion,
     'damerau_r':   damerau_restricted_edicion,
-    'damerau_i':   damerau_intermediate_edicion
+    #'damerau_i':   damerau_intermediate_edicion
 }
 
