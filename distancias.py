ebf0d4cc4ba0e8f9a1bd623a832e7fe48a107ef9
HakuMi <82086904+mire4@users.noreply.github.com>
2022-11-09 19:35:23 +0100

    Ampliaciones acabadas

--- distancias.py
+++ distancias.py
@@ -227,7 +227,7 @@ def damerau_restricted_edicion(x, y, threshold=None):
             indX += 1
             indY += 1
         elif (o == 'int'):
-            secOp.append((str(x[indX]) + str(x[indX + 1]), str(y[indY]) + str(y[indY + 1])))
+            secOp.append((str(x[indX:indX + 2]), str(y[indY:indY + 2])))
             indX += 2
             indY += 2
         #print(secOp)
@@ -239,7 +239,7 @@ def damerau_restricted(x, y, threshold=None):
     difX = lenX - lenY
     vcurrent = np.zeros(lenX, dtype=int)
     vprev = np.arange(1, lenX + 1, dtype=int)
-    vpreprev = 2147483647 * np.ones(lenX, dtype=int) # inf
+    vprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
     for j in range(lenY):
         for i in range(lenX):
             if (difX + j > 0 and vprev[difX + j - 1] > threshold):
@@ -265,25 +265,236 @@ def damerau_restricted(x, y, threshold=None):
                 else:
                     vcurrent[i] = min(
                         vcurrent[i],
-                        vpreprev[i - 2] + 1
+                        vprevprev[i - 2] + 1
                     )
-        vpreprev = vprev.copy()
+        vprevprev = vprev.copy()
         vprev, vcurrent = vcurrent, vprev
     return vprev[-1]
 
 def damerau_intermediate_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein intermedia con matriz
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1): # Rellena la primera fila
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1): # Para cada columna
+        D[0][j] = D[0][j - 1] + 1 # Rellenas su vertical
+        for i in range(1, lenX + 1): # Para cada fila
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+            if (i > 1 and j > 1)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 2] + 1
+                )
+
+            # acb -> ba
+            if (i > 2 and j > 1)\
+                and (x[i - 3] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 3][j - 2] + 2
+                )
+
+            # ba -> acb
+            if (i > 1 and j > 2)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 3]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 3] + 2
+                )
+                
     return D[lenX, lenY]
 
 def damerau_intermediate_edicion(x, y, threshold=None):
     # partiendo de matrix_intermediate_damerau añadir recuperar
     # secuencia de operaciones de edición
     # completar versión Damerau-Levenstein intermedia con matriz
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1): # Rellena la primera fila
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1): # Para cada columna
+        D[0][j] = D[0][j - 1] + 1 # Rellenas su vertical
+        for i in range(1, lenX + 1): # Para cada fila
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+            if (i > 1 and j > 1)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 2] + 1
+                )
+            #acb -> ba
+            if (i > 2 and j > 1)\
+                and (x[i - 3] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 3][j - 2] + 2
+                )
+
+            #ba -> acb
+            if (i > 1 and j > 2)\
+                and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 3]):
+                D[i][j] = min(
+                    D[i][j],
+                    D[i - 2][j - 3] + 2
+                )
+
+    indX, indY = D.shape[0] - 1, D.shape[1] - 1
+    op = []
+    while indX > 0 and indY > 0:
+        ins = D[indX, indY - 1]
+        bor = D[indX - 1, indY]
+        sus = D[indX - 1, indY - 1]
+        if (indX > 1 and indY > 1)\
+                and (x[indX - 2] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
+            int = D[indX - 2, indY - 2]
+        else:
+            int = float('inf')
+        if (indX > 2 and indY > 1)\
+                and (x[indX - 3] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
+            int3_2 = D[indX - 3][indY - 2]
+        else:
+            int3_2 = float('inf')
+        if (indX > 1 and indY > 2)\
+                and (x[indX - 2] == y[indY - 1] and x[indX - 1] == y[indY - 3]):
+            int2_3 = D[indX - 2][indY - 3]
+        else:
+            int2_3 = float('inf')
+        opMin = min(ins, bor, sus, int, int3_2, int2_3)
+        if (int3_2 == opMin):
+            op.append('int3_2')
+            indX -= 3
+            indY -= 2
+        elif (int2_3 == opMin):
+            op.append('int2_3')
+            indX -= 2
+            indY -= 3
+        elif (int == opMin):
+            op.append('int')
+            indX -= 2
+            indY -= 2
+        elif (sus == opMin):
+            op.append('sus')
+            indX -= 1
+            indY -= 1
+        elif (ins == opMin):
+            op.append('ins')
+            indY -= 1
+        elif (bor == opMin):
+            op.append('bor')
+            indX -= 1
+    # Caso llego a una pared de la matriz
+    while indY > 0:
+        op.append('ins')
+        indY -= 1
+    while indX > 0:
+        op.append('bor')
+        indX -= 1
+
+    # Recuperación del camino
+    op = op[::-1]
+    indX, indY = 0, 0
+    #print(op)
+    secOp = []
+    for o in op:
+        if (o == 'ins'):
+            secOp.append(('', y[indY]))
+            indY += 1
+        elif (o == 'bor'):
+            secOp.append((x[indX], ''))
+            indX += 1
+        elif (o == 'sus'):
+            secOp.append((x[indX], y[indY]))
+            indX += 1
+            indY += 1
+        elif (o == 'int'):
+            secOp.append((str(x[indX:indX + 2]), str(y[indY:indY + 2])))
+            indX += 2
+            indY += 2
+        elif (o == 'int3_2'):
+            secOp.append((str(x[indX:indX + 3]), str(y[indY:indY + 2])))
+            indX += 3
+            indY += 2
+        elif (o == 'int2_3'):
+            secOp.append((str(x[indX:indX + 2]), str(y[indY:indY + 3])))
+            indX += 2
+            indY += 3
+    return D[lenX, lenY], secOp
     
 def damerau_intermediate(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
-    return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    difX = lenX - lenY
+    vcurrent = np.zeros(lenX, dtype=int)
+    vprev = np.arange(1, lenX + 1, dtype=int)
+    vprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
+    vprevprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
+    for j in range(lenY):
+        for i in range(lenX):
+            if (difX + j > 0 and vprev[difX + j - 1] > threshold):
+                return threshold + 1
+            if (i == 0):
+                vcurrent[0] = min(
+                    j + (x[i] != y[j]),
+                    vprev[i] + 1
+                )
+            else:
+                vcurrent[i] = min(
+                    vcurrent[i - 1] + 1,
+                    vprev[i] + 1,
+                    vprev[i - 1] + (x[i] != y[j])
+                )
+            if ((i > 0 and j > 0)\
+                and (x[i - 1] == y[j] and x[i] == y[j - 1])):
+                if (i == 1):
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        j               # j - 1 + 1
+                    )
+                else:
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        vprevprev[i - 2] + 1
+                    )
+            #acb -> ba
+            if (i > 1 and j > 0)\
+                and (x[i - 2] == y[j] and x[i] == y[j - 1]):
+                if (i == 2):
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        j + 1           # j - 1 + 2
+                    )
+                else:
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        vprevprev[i - 3] + 2
+                    )
+            #ba -> acb
+            if (i > 0 and j > 1)\
+                and (x[i - 1] == y[j] and x[i] == y[j - 2]):
+                if (i == 1):
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        j               # j - 2 + 2
+                    )
+                else:
+                    vcurrent[i] = min(
+                        vcurrent[i],
+                        vprevprevprev[i - 2] + 2
+                    )
+        vprevprevprev = vprevprev.copy()
+        vprevprev = vprev.copy()
+        vprev, vcurrent = vcurrent, vprev
+    return vprev[-1]
 
 opcionesSpell = {
     'levenshtein_m': levenshtein_matriz,
@@ -292,13 +503,13 @@ opcionesSpell = {
     'levenshtein_o': levenshtein_cota_optimista,
     'damerau_rm':    damerau_restricted_matriz,
     'damerau_r':     damerau_restricted,
-    #'damerau_im':    damerau_intermediate_matriz,
-    #'damerau_i':     damerau_intermediate
+    'damerau_im':    damerau_intermediate_matriz,
+    'damerau_i':     damerau_intermediate
 }
 
 opcionesEdicion = {
     'levenshtein': levenshtein_edicion,
     'damerau_r':   damerau_restricted_edicion,
-    #'damerau_i':   damerau_intermediate_edicion
+    'damerau_i':   damerau_intermediate_edicion
 }
 
--- generar_resultados_SAR.bat
+++ generar_resultados_SAR.bat
@@ -1,7 +1,7 @@
 @echo off
-echo python .\SAR_Indexer.py -M data/2015 index2015.bin
+echo python .\SAR_Indexer.py data/2015 index2015.bin
 
-FOR %%x IN (levenshtein_o) DO (
+FOR %%x IN (levenshtein_o damerau_r damerau_i) DO (
     FOR %%y in (1 2 3) DO (
         python SAR_Searcher.py index2015.bin -L referenciasSAR/queries.txt -C -t %%y -d %%x > referenciasSAR/resul_%%x_%%y.txt
     )
