a118af8cfd0c64f50be6105124f75d11c4f7c254
CheesyNacho10 <67198608+CheesyNacho10@users.noreply.github.com>
2022-11-13 10:36:19 +0100

    Comentado distancias.py

--- distancias.py
+++ distancias.py
@@ -90,7 +90,10 @@ def levenshtein(x, y, threshold):
     vcurrent = np.zeros(lenX, dtype=int)
     vprev = np.arange(1, lenX + 1, dtype=int)
     for j in range(lenY):
-        if (difX + j > 0 and vprev[difX + j - 1] > threshold): # Comparación de la distancia con la cota
+        # Comparación de la distancia con la cota
+        # Suponemos que la diagonal de la solución 
+        # contiene las subdistancias <= distanciaFinal
+        if (difX + j > 0 and vprev[difX + j - 1] > threshold): 
             return threshold + 1
         for i in range(lenX):
             if (i == 0):
@@ -111,7 +114,7 @@ def levenshtein_cota_optimista(x, y, threshold):
     contDict = dict() # Creación de un diccionario
     distancePos, distanceNeg = 0, 0 
     for letter in x: # Para cada letra de la palabra X
-        contDict[letter] = contDict.get(letter, 0) + 1 # Se cuentan las veces que aparece cada palabra
+        contDict[letter] = contDict.get(letter, 0) + 1 # Se cuentan las veces que aparece cada letra
     for letter in y: # Para cada letra de la palabra Y
         contDict[letter] = contDict.get(letter, 0) - 1 # Se descuentan las veces que aparece cada letra
     for value in contDict.values(): 
@@ -124,7 +127,6 @@ def levenshtein_cota_optimista(x, y, threshold):
     return levenshtein(x, y, threshold) 
 
 def damerau_restricted_matriz(x, y, threshold=None):
-    # completar versión Damerau-Levenstein restringida con matriz
     lenX, lenY = len(x), len(y)
     D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
     for i in range(1, lenX + 1): # Rellena la primera fila
@@ -132,11 +134,13 @@ def damerau_restricted_matriz(x, y, threshold=None):
     for j in range(1, lenY + 1): # Para cada columna
         D[0][j] = D[0][j - 1] + 1 # Rellenas su vertical
         for i in range(1, lenX + 1): # Para cada fila
+            # Casos de levenshtein
             D[i][j] = min(
                 D[i - 1][j] + 1,
                 D[i][j - 1] + 1,
                 D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
             )
+            # Nueva condición de Damerau-restricted
             if (i > 1 and j > 1)\
                 and (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]):
                 D[i][j] = min(
@@ -147,8 +151,6 @@ def damerau_restricted_matriz(x, y, threshold=None):
     return D[lenX, lenY]
 
 def damerau_restricted_edicion(x, y, threshold=None):
-    # partiendo de damerau_restricted_matriz añadir recuperar
-    # secuencia de operaciones de edición
     lenX, lenY = len(x), len(y)
     D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
     for i in range(1, lenX + 1): # Rellena la primera fila
@@ -173,11 +175,15 @@ def damerau_restricted_edicion(x, y, threshold=None):
         ins = D[indX, indY - 1]
         bor = D[indX - 1, indY]
         sus = D[indX - 1, indY - 1]
+
+        # Si no se cumple la confición entonces
+        # tiene valor infinito la distancia
         if (indX > 1 and indY > 1)\
                 and (x[indX - 2] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
             int = D[indX - 2, indY - 2]
         else:
             int = float('inf')
+
         op, decX, decY = elegir_operacion(x, y, indX, indY, ins, bor, sus, int)
         secOp.append(op)
         indX -= decX
@@ -192,14 +198,16 @@ def damerau_restricted_edicion(x, y, threshold=None):
     return D[lenX, lenY], secOp
 
 def damerau_restricted(x, y, threshold=None):
-    # versión con reducción coste espacial y parada por threshold
     lenX, lenY = len(x), len(y)
     difX = lenX - lenY
+    # Utilizamos ahora un tercer vector inicializado 
+    # a valores similares a infinito
     vcurrent = np.zeros(lenX, dtype=int)
     vprev = np.arange(1, lenX + 1, dtype=int)
-    vprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
+    vpenult = 2147483647 * np.ones(lenX, dtype=int) # inf
     for j in range(lenY):
         for i in range(lenX):
+            # Condición de parada
             if (difX + j > 0 and vprev[difX + j - 1] > threshold):
                 return threshold + 1
             if (i == 0):
@@ -213,8 +221,10 @@ def damerau_restricted(x, y, threshold=None):
                     vprev[i] + 1,
                     vprev[i - 1] + (x[i] != y[j])
                 )
+            # Condición de damerau
             if ((i > 0 and j > 0)\
                 and (x[i - 1] == y[j] and x[i] == y[j - 1])):
+                # Caso base
                 if (i == 1):
                     vcurrent[i] = min(
                         vcurrent[i],
@@ -223,14 +233,15 @@ def damerau_restricted(x, y, threshold=None):
                 else:
                     vcurrent[i] = min(
                         vcurrent[i],
-                        vprevprev[i - 2] + 1
+                        vpenult[i - 2] + 1
                     )
-        vprevprev, vprev = vprev, vprevprev
+        # Intercambiamos las referencias para que
+        # vpenult = vprev, vprev = vcurrent
+        vpenult, vprev = vprev, vpenult
         vprev, vcurrent = vcurrent, vprev
     return vprev[-1]
 
 def damerau_intermediate_matriz(x, y, threshold=None):
-    # completar versión Damerau-Levenstein intermedia con matriz
     lenX, lenY = len(x), len(y)
     D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
     for i in range(1, lenX + 1): # Rellena la primera fila
@@ -250,6 +261,7 @@ def damerau_intermediate_matriz(x, y, threshold=None):
                     D[i - 2][j - 2] + 1
                 )
 
+            # Nuevos casos de damerau_intermediate
             # acb -> ba
             if (i > 2 and j > 1)\
                 and (x[i - 3] == y[j - 1] and x[i - 1] == y[j - 2]):
@@ -269,9 +281,6 @@ def damerau_intermediate_matriz(x, y, threshold=None):
     return D[lenX, lenY]
 
 def damerau_intermediate_edicion(x, y, threshold=None):
-    # partiendo de matrix_intermediate_damerau añadir recuperar
-    # secuencia de operaciones de edición
-    # completar versión Damerau-Levenstein intermedia con matriz
     lenX, lenY = len(x), len(y)
     D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
     for i in range(1, lenX + 1): # Rellena la primera fila
@@ -290,6 +299,8 @@ def damerau_intermediate_edicion(x, y, threshold=None):
                     D[i][j],
                     D[i - 2][j - 2] + 1
                 )
+
+            # Nuevos casos de damerau_intermediate
             #acb -> ba
             if (i > 2 and j > 1)\
                 and (x[i - 3] == y[j - 1] and x[i - 1] == y[j - 2]):
@@ -312,11 +323,14 @@ def damerau_intermediate_edicion(x, y, threshold=None):
         ins = D[indX, indY - 1]
         bor = D[indX - 1, indY]
         sus = D[indX - 1, indY - 1]
+
         if (indX > 1 and indY > 1)\
                 and (x[indX - 2] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
             int = D[indX - 2, indY - 2]
         else:
             int = float('inf')
+        # Realizamos lo mismo para las nuevas 
+        # condiciones de damerau
         if (indX > 2 and indY > 1)\
                 and (x[indX - 3] == y[indY - 1] and x[indX - 1] == y[indY - 2]):
             int3_2 = D[indX - 3][indY - 2]
@@ -327,6 +341,7 @@ def damerau_intermediate_edicion(x, y, threshold=None):
             int2_3 = D[indX - 2][indY - 3]
         else:
             int2_3 = float('inf')
+
         op, decX, decY = elegir_operacion(x, y, indX, indY, ins, bor, sus, int, int3_2, int2_3)
         secOp.append(op)
         indX -= decX
@@ -341,15 +356,17 @@ def damerau_intermediate_edicion(x, y, threshold=None):
     return D[lenX, lenY], secOp
     
 def damerau_intermediate(x, y, threshold=None):
-    # versión con reducción coste espacial y parada por threshold
     lenX, lenY = len(x), len(y)
     difX = lenX - lenY
+    # Utilizamos un vector adicional para 
+    # los nuevos casos de damerau_intermediate
     vcurrent = np.zeros(lenX, dtype=int)
     vprev = np.arange(1, lenX + 1, dtype=int)
-    vprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
-    vprevprevprev = 2147483647 * np.ones(lenX, dtype=int) # inf
+    vpenult = 2147483647 * np.ones(lenX, dtype=int) # inf
+    vantepenult = 2147483647 * np.ones(lenX, dtype=int) # inf
     for j in range(lenY):
         for i in range(lenX):
+            # Parada por threshold
             if (difX + j > 0 and vprev[difX + j - 1] > threshold):
                 return threshold + 1
             if (i == 0):
@@ -373,9 +390,12 @@ def damerau_intermediate(x, y, threshold=None):
                 else:
                     vcurrent[i] = min(
                         vcurrent[i],
-                        vprevprev[i - 2] + 1
+                        vpenult[i - 2] + 1
                     )
-            #acb -> ba
+
+            # Nuevos casos de damerau_intermediate
+            # con sus casos base
+            # acb -> ba
             if (i > 1 and j > 0)\
                 and (x[i - 2] == y[j] and x[i] == y[j - 1]):
                 if (i == 2):
@@ -386,9 +406,9 @@ def damerau_intermediate(x, y, threshold=None):
                 else:
                     vcurrent[i] = min(
                         vcurrent[i],
-                        vprevprev[i - 3] + 2
+                        vpenult[i - 3] + 2
                     )
-            #ba -> acb
+            # ba -> acb
             if (i > 0 and j > 1)\
                 and (x[i - 1] == y[j] and x[i] == y[j - 2]):
                 if (i == 1):
@@ -399,14 +419,19 @@ def damerau_intermediate(x, y, threshold=None):
                 else:
                     vcurrent[i] = min(
                         vcurrent[i],
-                        vprevprevprev[i - 2] + 2
+                        vantepenult[i - 2] + 2
                     )
-        vprevprevprev, vprevprev = vprevprev, vprevprevprev
-        vprevprev, vprev = vprev, vprevprev
+        vantepenult, vpenult = vpenult, vantepenult
+        vpenult, vprev = vprev, vpenult
         vprev, vcurrent = vcurrent, vprev
     return vprev[-1]
 
 def elegir_operacion(x, y, indX, indY, dIns, dBor, dSus, dInt=float('inf'), dInt3_2=float('inf'), dInt2_3=float('inf')):
+    """
+        Método auxiliar que pretende servir de 
+        switch para mayor claridad del código
+        en las recuperaciones de edición
+    """
     opMin = min(dIns, dBor, dSus, dInt, dInt3_2, dInt2_3)
     if (dInt3_2 == opMin): # Intercambio 3 a 2 letras
         return (str(x[indX - 3:indX]), str(y[indY - 2:indY])), 3, 2
